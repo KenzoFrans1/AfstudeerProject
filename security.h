74a6924a4d7e279a3c81cd164366b1968c0a843c
KenzoFrans1 <kenzo.frans@telenet.be>
2025-06-07 16:11:30 +0200

    Add files via upload

---
+++ security.h
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+// WEB AUTHENTICATION CREDENTIALS
+const char* web_username = "admin";
+const char* web_password = "admin";
+
+// WIFI
+// const char* ssid = "IoT";
+// const char* password = "KdGIoT83!";
+
+// const char* ssid = "telenet-1683985";
+// const char* password = "Ffncutzz8z5X";
+
+const char* ssid = "IoT2";
+const char* password = "KdGIoT69!";
+
+// Airtable API 
+const String airtableBaseId = "appQWGeeDa9jTkjA0";
+const String airtableApiKey = "patFt9oQiirC1e7Hl.7bf0a857deec3d64d3073c1bd75baa48d59808bca8576ded6c3c4a16f2f2c496";
